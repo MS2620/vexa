96ae96c3faa97503e396263394b7afbd475184ef
MS2620 <113364931+MS2620@users.noreply.github.com>
2026-03-17 21:13:34 +0000

    feat: enhance MediaCard component with improved image URL handling and update styles

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   output: "standalone",
   images: {
     domains: ["image.tmdb.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+    ],
   },
 };
 
